eaaba3db286a1a4f2b5536bad0d8611e9879b41b
Leonardo Duarte - RA 12105384 <>
2020-05-06 20:50:00 -0300

    feat: cria projeto

---
+++ src/main/java/br/lgomesd/gestaotarefas/Tarefa.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.lgomesd.gestaotarefas;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+/**
+ *
+ * @author lgome
+ */
+@Entity
+@Table(name = "tb_tarefa")
+public class Tarefa implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tarefa")
+    private Integer id;
+    @Column(name = "nom_tarefa", nullable = false, unique = true, length = 500)
+    private String nome;
+    
+    @Column(name = "nom_prioridade", nullable = false, length = 50)
+    private String prioridade;
+    
+    @Column(name = "num_percentual", nullable = false)
+    private Integer percentual;
+    
+    @Column (name= "des_descricao")
+    private String descricao;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "dta_limite")
+    private Calendar dataLimite;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+    
+        /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 73 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Tarefa other = (Tarefa) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" + "id=" + id + ", nome=" + nome + '}';
+    }
+
+    /**
+     * @return the prioridade
+     */
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    /**
+     * @param prioridade the prioridade to set
+     */
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    /**
+     * @return the percentual
+     */
+    public Integer getPercentual() {
+        return percentual;
+    }
+
+    /**
+     * @param percentual the percentual to set
+     */
+    public void setPercentual(Integer percentual) {
+        this.percentual = percentual;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the dataLimite
+     */
+    public Calendar getDataLimite() {
+        return dataLimite;
+    }
+
+    /**
+     * @param dataLimite the dataLimite to set
+     */
+    public void setDataLimite(Calendar dataLimite) {
+        this.dataLimite = dataLimite;
+    }
+
+   
+
+
+    
+    
+    
+}
